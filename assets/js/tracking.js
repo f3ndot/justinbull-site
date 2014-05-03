8ee7a31e080c21c640ad59afa86fb43460a113ee
Justin Bull <me@justinbull.ca>
2014-05-03 09:06:36 -0400

    Enhance tracking

--- assets/js/tracking.js
+++ assets/js/tracking.js
@@ -4,4 +4,6 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-36829887-3', 'justinbull.ca');
+ga('require', 'displayfeatures');
+ga('require', 'linkid', 'linkid.js');
 ga('send', 'pageview');
